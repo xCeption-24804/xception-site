c3c07843e29317e6b2504f6778b2e5993ccc769f
Darkhan Karabayev <dark010oqo@gmail.com>
2026-03-02 22:44:00 +0500

    init react

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  // tailwindcss
+  tailwindAttributes: ["theme"],
+  tailwindFunctions: ["twMerge", "createTheme"],
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { MyNavbar } from "./layouts/MyNavbar";
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+    </>
+  );
+}
+
+export default App;
